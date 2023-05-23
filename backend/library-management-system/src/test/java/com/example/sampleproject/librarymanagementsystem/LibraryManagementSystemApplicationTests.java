ac99bc67c50ba8d3401c8a9bff5c91ece587acd8
rahulhere-1 <rmendigeri4@gmail.com>
2023-05-23 14:28:00 +0530

    first commit of the project

---
+++ backend/library-management-system/src/test/java/com/example/sampleproject/librarymanagementsystem/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleproject.librarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
