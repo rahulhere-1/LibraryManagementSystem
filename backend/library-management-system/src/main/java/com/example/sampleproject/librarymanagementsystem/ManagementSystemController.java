2d9b6e548c4be55176f9d9545e7d36398b3bd470
rahulhere-1 <rmendigeri4@gmail.com>
2023-06-04 18:48:01 +0530

    added service layer

--- backend/library-management-system/src/main/java/com/example/sampleproject/librarymanagementsystem/ManagementSystemController.java
+++ backend/library-management-system/src/main/java/com/example/sampleproject/librarymanagementsystem/ManagementSystemController.java
@@ -31,106 +31,109 @@ public class ManagementSystemController {
 	@Autowired
 	private BorrowedJpaRepository borrowedRepository;
 	
+	@Autowired
+	private SystemService service;
+	
 	@GetMapping(path="/library")
 	public List<Book> retrieveAllBooks() {
-		return bookRepository.findAll();
+		return service.retrieveAllBooks();
 	}
 	
 	@PostMapping(path="/library")
 	public void addBooks(@RequestBody Book book) {
-		bookRepository.save(book);
+		service.addBooks(book);
 	}
 	
 	@PutMapping(path="/library")
 	public void updateThisBooks(@RequestBody Book book) {
-		bookRepository.save(book);
+		service.updateThisBooks(book);
 	}
 	
 
 	
 	@GetMapping(path="/library/{bookID}")
 	public Optional<Book> findThisBook(@PathVariable String bookID ) {
-		return bookRepository.findById(bookID);
+		return service.findThisBook(bookID);
 	}
 	
 	@DeleteMapping(path="/library/{bookID}")
 	public void deleteThisBook(@PathVariable String bookID ) {
-		bookRepository.deleteById(bookID);
+		service.deleteThisBook(bookID);
 	}
 	
 	
 	
 	@GetMapping(path="/library/author/{author}")
 	public List<Book> findBookByAuthor(@PathVariable String author ) {
-		return bookRepository.findByAuthor(author);
+		return service.findBookByAuthor(author);
 	}
 	
 	
 	@GetMapping(path="/members")
 	public List<Member> retrieveAllMembers() {
-		return memberRepository.findAll();
+		return service.retrieveAllMembers();
 	}
 	
 	@PostMapping(path="/members")
 	public void addMembers(@RequestBody Member member) {
-		memberRepository.save(member);
+		service.addMembers(member);
 	}
 	
 	@PutMapping(path="/members")
 	public void updateThisMembers(@RequestBody Member member) {
-		memberRepository.save(member);
+		service.updateThisMembers(member);
 	}
 	
 	@GetMapping(path="/members/{memberID}")
 	public Optional<Member> retrieveAllMembers(@PathVariable Long memberID) {
-		return memberRepository.findById(memberID);
+		return service.retrieveAllMembers(memberID);
 	}
 	
 	@DeleteMapping(path="/members/{memberID}")
 	public void removeThisMember(@PathVariable Long memberID) {
-		 memberRepository.deleteById(memberID);
+		 service.removeThisMember(memberID);
 	}
 	
 	@GetMapping(path="/borrowed")
 	public List<Borrowed> retrieveAllBorrowers() {
-		return borrowedRepository.findAll();
+		return service.retrieveAllBorrowers();
 	}
 	
 	@PostMapping(path="/borrowed")
 	public void addBorrowers(@RequestBody Borrowed borrower) {
-		 borrowedRepository.save(borrower);
+		 service.addBorrowers(borrower);
 	}
 	
 
 	@PutMapping(path="/borrowed")
 	public void updateBorrowers(@RequestBody Borrowed borrower) {
-		 borrowedRepository.save(borrower);
+		 service.updateBorrowers(borrower);
 	}
 	
 	
 	@GetMapping(path="/borrowed/isbn/{isbn}")
 	public List<Borrowed> getByIsbn(@PathVariable String isbn) {
-		return borrowedRepository.findByBookIsbn(isbn);
+		return service.getByIsbn(isbn);
 	}
 	
 	@GetMapping(path="/borrowed/member/{id}")
 	public List<Borrowed> getByMemberId(@PathVariable Long id) {
-		return borrowedRepository.findByMemberId(id);
+		return service.getByMemberId(id);
 	}
 	
 	@DeleteMapping(path="/borrowed/member/{id}")
 	public void removeIssuedBookByMember(@PathVariable Long id) {
-		 borrowedRepository.deleteByMemberId(id);
+		 service.removeIssuedBookByMember(id);
 	}
 	
 	@DeleteMapping(path="/borrowed/{id}")
 	public void removeIssuedBookById(@PathVariable Long id) {
-		borrowedRepository.deleteById(id);
+		service.removeIssuedBookById(id);
 	}
 	
 	@DeleteMapping(path="/borrowed/isbn/{isbn}")
 	public void removeIssuedBookById(@PathVariable String isbn) {
-		 borrowedRepository.deleteByBookIsbn(isbn);
+		 service.removeIssuedBookById(isbn);
 	}
 	
 }
---
+++ backend/library-management-system/src/main/java/com/example/sampleproject/librarymanagementsystem/SystemService.java
@@ -0,0 +1,133 @@
+package com.example.sampleproject.librarymanagementsystem;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.sampleproject.librarymanagementsystem.books.Book;
+import com.example.sampleproject.librarymanagementsystem.books.BooksJpaRepository;
+import com.example.sampleproject.librarymanagementsystem.borrowedby.Borrowed;
+import com.example.sampleproject.librarymanagementsystem.borrowedby.BorrowedJpaRepository;
+import com.example.sampleproject.librarymanagementsystem.members.Member;
+import com.example.sampleproject.librarymanagementsystem.members.MembersJpaRepository;
+
+@Service
+public class SystemService {
+	
+
+	@Autowired
+	private BooksJpaRepository bookRepository;
+	
+	@Autowired
+	private MembersJpaRepository memberRepository;
+	
+	@Autowired
+	private BorrowedJpaRepository borrowedRepository;
+	
+
+	public List<Book> retrieveAllBooks() {
+		return bookRepository.findAll();
+	}
+	
+	
+	public void addBooks( Book book) {
+		bookRepository.save(book);
+	}
+	
+	
+	public void updateThisBooks( Book book) {
+		bookRepository.save(book);
+	}
+	
+
+	
+
+	public Optional<Book> findThisBook( String bookID ) {
+		return bookRepository.findById(bookID);
+	}
+	
+
+	public void deleteThisBook( String bookID ) {
+		bookRepository.deleteById(bookID);
+	}
+	
+	
+	
+
+	public List<Book> findBookByAuthor( String author ) {
+		return bookRepository.findByAuthor(author);
+	}
+	
+	
+	public List<Member> retrieveAllMembers() {
+		return memberRepository.findAll();
+	}
+	
+	
+	public void addMembers( Member member) {
+		memberRepository.save(member);
+	}
+	
+
+	public void updateThisMembers( Member member) {
+		memberRepository.save(member);
+	}
+	
+
+	public Optional<Member> retrieveAllMembers( Long memberID) {
+		return memberRepository.findById(memberID);
+	}
+	
+
+	public void removeThisMember( Long memberID) {
+		 memberRepository.deleteById(memberID);
+	}
+	
+
+	public List<Borrowed> retrieveAllBorrowers() {
+		return borrowedRepository.findAll();
+	}
+
+	public void addBorrowers( Borrowed borrower) {
+		 borrowedRepository.save(borrower);
+	}
+	
+
+	public void updateBorrowers( Borrowed borrower) {
+		 borrowedRepository.save(borrower);
+	}
+	
+	
+	public List<Borrowed> getByIsbn( String isbn) {
+		return borrowedRepository.findByBookIsbn(isbn);
+	}
+
+
+	public List<Borrowed> getByMemberId( Long id) {
+		return borrowedRepository.findByMemberId(id);
+	}
+
+
+	public void removeIssuedBookByMember( Long id) {
+		 borrowedRepository.deleteByMemberId(id);
+	}
+
+
+	public void removeIssuedBookById( Long id) {
+		borrowedRepository.deleteById(id);
+	}
+
+
+	public void removeIssuedBookById( String isbn) {
+		 borrowedRepository.deleteByBookIsbn(isbn);
+	}
+
+}
